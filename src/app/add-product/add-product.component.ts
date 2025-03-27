ab74a0509620cb3a1340dfd1977d6271ec7c5b47
fadihboubati <eng.fadi.hb@gmail.com>
2025-03-27 00:15:26 -0400

    refactor

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ProductService } from '../product.service';
+import { ProductService } from '../services/product.service/product.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
+import { ProductService } from '../services/product.service/product.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
